dca7250cda487619bf1f440533cdacc16bcad5f1
Issue-03 <ishucuriosity@gmail.com>
2020-07-03 20:05:29 +0530

    Practice Day 3

---
+++ Strings/Longest substring without repeating characters.java
@@ -0,0 +1,62 @@
+// Given a string, find the length of the longest substring without repeating characters.
+
+// Example 1:
+// Input: "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+// Input: "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Approach: Sliding Window
+public class Solution {
+
+  public int lengthOfLongestSubstring(String s) {
+    int n = s.length();
+    Set<Character> set = new HashSet<>();
+    int ans = 0, i = 0, j = 0;
+    while (i < n && j < n) {
+      // try to extend the range [i, j]
+      if (!set.contains(s.charAt(j))) {
+        set.add(s.charAt(j++));
+        ans = Math.max(ans, j - i);
+      } else {
+        set.remove(s.charAt(i++));
+      }
+    }
+    return ans;
+  }
+}
+
+// Approach: Sliding Window Optimized
+public class Solution {
+
+  public int lengthOfLongestSubstring(String s) {
+    int n = s.length(), ans = 0;
+    Map<Character, Integer> map = new HashMap<>();
+    for (int j = 0, i = 0; j < n; j++) {
+      if (map.containsKey(s.charAt(j))) {
+        i = Math.max(map.get(s.charAt(j)), i);
+      }
+      ans = Math.max(ans, j - i + 1);
+      map.put(s.charAt(j), j + 1);
+    }
+    return ans;
+  }
+}
+
+public class Solution {
+
+  public int lengthOfLongestSubstring(String s) {
+    int n = s.length(), ans = 0;
+    int[] index = new int[128];
+    for (int j = 0, i = 0; j < n; j++) {
+      i = Math.max(index[s.charAt(j)], i);
+      ans = Math.max(ans, j - i + 1);
+      index[s.charAt(j)] = j + 1;
+    }
+    return ans;
+  }
+}
