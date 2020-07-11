8d2e7e48db4153798c988cf34b53b8311c755033
Issue-03 <ishucuriosity@gmail.com>
2020-07-11 21:03:05 +0530

    Practice Day 11

---
+++ Bit Masking/Subsets.java
@@ -0,0 +1,58 @@
+// Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+// Note: The solution set must not contain duplicate subsets.
+
+// Example:
+
+// Input: nums = [1,2,3]
+// Output:
+// [
+//   [3],
+//   [1],
+//   [2],
+//   [1,2,3],
+//   [1,3],
+//   [2,3],
+//   [1,2],
+//   []
+// ]
+
+class Solution {
+  public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> output = new ArrayList();
+    int n = nums.length;
+      
+    for(int i=0;i<(1<<n);i++) {
+        List<Integer> curr = new ArrayList();
+        for(int j=0;j<n;j++) {
+            if((i & (1<<j)) > 0) {
+            curr.add(nums[j]);
+        }
+            
+        }
+        output.add(curr);
+    }  
+        return output;
+    }  
+  }
+
+
+class Solution {
+  public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> output = new ArrayList();
+    int n = nums.length;
+
+    for (int i = (int)Math.pow(2, n); i < (int)Math.pow(2, n + 1); ++i) {
+      // generate bitmask, from 0..00 to 1..11
+      String bitmask = Integer.toBinaryString(i).substring(1);
+
+      // append subset corresponding to that bitmask
+      List<Integer> curr = new ArrayList();
+      for (int j = 0; j < n; ++j) {
+        if (bitmask.charAt(j) == '1') curr.add(nums[j]);
+      }
+      output.add(curr);
+    }
+    return output;
+  }
+}
